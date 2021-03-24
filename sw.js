cd3fe1db9a6e36be49375377741d54d3ef688292
Xiaopeng Zhu <nimezhu@gmail.com>
2021-03-24 06:26:46 -0400

    Updates

---
+++ sw.js
@@ -0,0 +1,70 @@
+importScripts("cache-polyfill.js")
+
+var PREFIX = "text2pic"
+var CACHE_VERSION = 'text2pic-v0.0.1';
+var CACHE_FILES = [
+    "./manifest.json",
+    "./lib.js", 
+    "./index.html", 
+    "./img/icon-192.png",
+    "./img/icon-512.png"
+];
+
+const cacheResources = async () => {
+  const cache = await caches.open(CACHE_VERSION)
+  return cache.addAll(CACHE_FILES)
+}
+
+self.addEventListener('install', event =>
+  event.waitUntil(cacheResources())
+)
+const myfetch = async req => {
+}
+
+
+const cachedResource = async req => {
+  const cache = await caches.open(CACHE_VERSION)
+  return await cache.match(req, {'ignoreSearch': true} ).then(function(response) {
+                return response || fetch(req).then(function(response) {
+                    return response;
+                }).catch(function(e){
+                    return "not found";
+                });
+    })
+}
+
+self.addEventListener('fetch', event =>
+  event.respondWith(cachedResource(event.request))
+)
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(keys.map(function(key, i) {
+                var a = key.split("-")
+                if (a[0] !== PREFIX) {
+                    return 
+                }
+                if (key !== CACHE_VERSION) {
+                    return caches.delete(keys[i]);
+                }
+            }))
+        })
+    )
+});
+
+self.addEventListener('message', function(event) {
+    if (event.data.action === 'skipWaiting') {
+        try {
+        console.log("calling skipWaiting")
+        self.skipWaiting();
+        } catch(e) {
+            console.log(e)
+        }   
+    }
+    //VERSION API STANDARD For ServiceWorkerBar
+    if (event.data.command == "version") {
+        event.ports[0].postMessage({
+            "version":CACHE_VERSION
+        })
+    }
+});
